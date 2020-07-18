0d6c3b2506cb6a40b2785fae33d9511fa3985015
abhirahul96 <abhi.rahulnayak@gmail.com>
2020-07-18 15:34:37 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const notes=require('./notes')
+
+console.log(notes())
+
+
+
+
+// const fs=require('fs')
+// fs.writeFileSync('notes.txt','My name is Abhi')
+// fs.appendFileSync('notes.txt',' Hello World')
